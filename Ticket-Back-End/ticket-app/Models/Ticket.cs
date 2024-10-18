f43ba86cdfc194e3dd2333fd7c3cbef2101b3d74
Mouad <mouad.fellak6@gmail.com>
2024-10-18 13:06:10 +0100

    repo

---
+++ Ticket-Back-End/ticket-app/Models/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Ticket_App.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TicketStatus Status { get; set; } 
+        public DateTime CreatedDate { get; set; }
+    }
+}
